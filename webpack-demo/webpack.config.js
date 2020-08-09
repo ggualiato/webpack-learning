501c18e2fd55a6b2b034082945d9da5793be434c
Giovanni Gualiato <giovanni.gualiato@hexagon.com>
2020-08-09 02:33:33 -0300

    Development section completed

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -3,18 +3,24 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+    mode: "development",
     entry: {
         app: "./src/index.js",
         print: "./src/print.js",
     },
+    devtool: "inline-source-map",
+    devServer: {
+        contentBase: "./dist",
+    },
     plugins: [
-        new CleanWebpackPlugin(),
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
         new HtmlWebpackPlugin({
-            title: "Output Management",
+            title: "Development",
         }),
     ],
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist"),
+        publicPath: "/",
     },
 };
